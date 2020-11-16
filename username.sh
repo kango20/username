e4bdcb8e1065150f03b11bde6c58705136830b0e
Karen Ngo <kango@chapman.edu>
2020-11-15 18:52:28 -1000

    Adds username.sh

--- username.sh
+++ username.sh
@@ -10,7 +10,7 @@ echo "Username must contain at least 3 but not more than 12 characters"
 read USERNAME
 while echo $USERNAME | egrep -v "^[a-z][0-9_a-z]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username - 3-12 characters, digits, and underscores only!"
+	echo "You must enter a valid username - 3-12 characters, digits, and underscores only and must start with a lower case character!"
 	echo "Enter a username: "
 	read USERNAME
 done
